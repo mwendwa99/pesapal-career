ddd379ace7f3cb369609231e731e2644fc08cf0b
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2022-01-18 13:54:47 +0300

    removed all packages. add tcp stream responses

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const http2 = require('http2');
+const fs = require('fs');
 
 // create a new session with google.com
-const session = http2.connect('http://localhost:8443');
+const session = http2.connect('https://localhost:8443', {
+    // since certificate is not signed by a trusted CA,
+    // we need to pass the certificate to the server
+    ca: fs.readFileSync('server.cert')
+});
 
 // log any errors to the console
 session.on('error', (err) => console.error(err));
-
-// // new request stream
-// const req = session.request({ ':path': '/' });
-// // end request
-// req.end();
 
 // set request timeout to 100ms
 const req = session.request({ ':path': '/', ':method': 'POST', ':scheme': 'https' });
 req.setTimeout(100, () => {
     // if the request times out, end the request
+    alert('request timed out!');
     console.log('request timed out');
     req.end();
 });
--- router.js
+++ router.js
@@ -1,24 +1,60 @@
+const fs = require('fs')
+
 // this is our original handler, extracted as a function
+// returns a response of 26 bytes
 const helloWorldHandler = (stream, headers) => {
     console.log({ headers })
     stream.respond({
+        'content-type': 'text/html; charset=utf-8',
+        'content-length': '26',
         ':status': 200
     })
-    stream.end('Hello World from router')
+    // send html file
+    stream.end(fs.readFileSync(__dirname + '/index.html'));
 }
 
-// the pingHandler returns "response success" to let us know that
-// the server is up and running
-const pingHandler = (stream, headers) => {
+// post request handler
+const getMessageBody = (stream, headers) => {
     console.log({ headers })
     // if stream has been closed, return
     if (stream.closed) {
         return
     }
-    stream.respond({
-        ':status': 200
+    // get the message body
+    const body = []
+    stream.on('data', (chunk) => {
+        body.push(chunk)
     })
-    stream.end('response success')
+    stream.on('end', () => {
+        // convert the body to a string
+        const message = Buffer.concat(body).toString()
+
+        // remove the + from the message
+        const messageWithoutPlus = message.replace(/\+=/g, ' ')
+
+        // respond with a message
+        stream.respond({
+            'content-type': 'text/html; charset=utf-8',
+            'content-length': '26',
+            ':status': 200
+        })
+        stream.end(`
+        <div style="
+                    background-color: #f0f0f0;
+                    padding: 10px; 
+                    display: flex; 
+                    justify-content: center;
+                    flex-direction: column;
+                    text-align: center; 
+                    align-items:center;">
+            <h1>This is a dynamically rendered page</h1>
+            <br />
+            <h3>your ${messageWithoutPlus}</h3>
+            <br />
+            <h3>go back to the <a href="/">home page</a></h3>
+        </div>
+        `)
+    })
 }
 
 // in case a route doesn't exist, we want to return a
@@ -44,11 +80,12 @@ const router = (stream, headers) => {
     // final destination for the request stream and assign
     // it to the `handler` variable
     let handler
-    if (path === "/hello-world" && method === 'GET') {
+    if (path === "/" && method === 'GET') {
         handler = helloWorldHandler
     }
-    else if (path === "/ping" && method == 'GET') {
-        handler = pingHandler
+    else if (path === "/message" && method == 'POST') {
+        // handler = messageHandler
+        handler = getMessageBody
     }
     else {
         handler = notFoundHandler
